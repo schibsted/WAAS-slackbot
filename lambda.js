89e968361f801b2602133687421a71c83791b4eb
Johannes Gorset <johannes.gorset@schibsted.com>
2022-10-13 14:28:21 +0200

    Fix stupid bugs

--- lambda.js
+++ lambda.js
@@ -1,12 +1,10 @@
 exports.handler = async (event, context) => {
   // Respond to challenge to verify bot with Slack
-  if (event.queryStringParameters.get('challenge')) {
+  if (event.queryStringParameters.challenge) {
     return {
       statusCode: 200,
       body: event.queryStringParameters.challenge
     };
   }
-
-
-
 }
+
